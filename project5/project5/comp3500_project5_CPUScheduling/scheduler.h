bc867e001d58f8601418d6d5ea467ce745372ecb
Colin Vande Vijvere <60828613+colinVDV@users.noreply.github.com>
2021-04-19 20:24:50 -0500

    Update scheduler.h

--- project5/project5/comp3500_project5_CPUScheduling/scheduler.h
+++ project5/project5/comp3500_project5_CPUScheduling/scheduler.h
@@ -5,11 +5,8 @@ Handler file for scheduler.c
 #define _SCHEDULER_H_
 
 stats* fcfs_policy(task_t task_array[], stats stats_array[], int count);
-
 stats* rr_policy(task_t task_array[], stats stats_array[], int finish_array[], int count, int time_quantum);
-
 stats* srtf_policy(task_t task_array[], stats stats_array[], int finish_array[], int count);
-
 stats* averageCalculator(stats stats_array[], int count);
 
 #endif
