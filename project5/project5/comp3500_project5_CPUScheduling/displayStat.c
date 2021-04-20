b1a0bf296e4b3881c12d581cfa22c0e1bd8bfed9
Colin Vande Vijvere <60828613+colinVDV@users.noreply.github.com>
2021-04-19 20:19:02 -0500

    Update displayStat.c

--- project5/project5/comp3500_project5_CPUScheduling/displayStat.c
+++ project5/project5/comp3500_project5_CPUScheduling/displayStat.c
@@ -1,14 +1,13 @@
 /*********************************************************************************
 * Display method
 **********************************************************************************/
-
 #include "input.h"
 
 void displayStat(float averageWaitingTime, float averageTurnaroundTime, float averageResponseTime) {
-    printf("==================================================================\n");    
+    printf("---------------------------------------------------------------\n");    
     printf("Average waiting time=\t\t%f\n", averageWaitingTime);
     printf("Average response time=\t\t%f\n", averageResponseTime);
     printf("Average turnaround time=\t%f\n", averageTurnaroundTime);
     printf("Overall CPU usage=\t\t100.00\n");
-    printf("==================================================================\n");
+    printf("---------------------------------------------------------------\n");
 }
